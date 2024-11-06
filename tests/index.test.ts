526c3e8e89124e4817b0ed800b6e779d080782e9
babakfp <babak.bxf@gmail.com>
2024-11-07 00:22:06 +0330

    chore: add a bunch of code for testing

---
+++ tests/index.test.ts
@@ -0,0 +1,73 @@
+import {
+    getCollectionEntries,
+    useCollections,
+    z,
+    type ImportGlobMarkdownMap,
+} from "../src"
+
+const pages = {
+    "/src/content/posts/hello-world.md": {
+        default: undefined as any,
+        mdx: {
+            frontmatter: {
+                title: "Hello, World!",
+            },
+        },
+        frontmatter: {
+            title: "Hello, World!",
+        },
+    },
+    "/src/content/products/hello-world.md": {
+        default: undefined as any,
+        mdx: {
+            frontmatter: {
+                title: "Hello, World!",
+            },
+        },
+        frontmatter: {
+            title: "Hello, World!",
+        },
+    },
+} satisfies ImportGlobMarkdownMap
+
+const postEntries = getCollectionEntries(
+    pages,
+    "posts",
+    z.object({
+        title: z.string(),
+    }),
+)
+
+const productsEntries = getCollectionEntries(
+    pages,
+    "products",
+    z.object({
+        title: z.string(),
+        price: z.number(),
+    }),
+)
+
+console.log(postEntries[0].frontmatter)
+console.log(productsEntries[0].frontmatter)
+
+// ---
+
+const collections = useCollections(pages)
+
+const postEntries2 = collections.getEntries(
+    "posts",
+    z.object({
+        title: z.string(),
+    }),
+)
+
+const productsEntries2 = collections.getEntries(
+    "products",
+    z.object({
+        title: z.string(),
+        price: z.number(),
+    }),
+)
+
+console.log(postEntries2[0].frontmatter)
+console.log(productsEntries2[0].frontmatter)
